82a972307c193db5f90f18cc04575b263440356f
Anastasiya Sachko <naztya12323@gmail.com>
2022-05-02 19:43:09 +0300

    feat: add solution to the task Carbon-dating

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -17,9 +17,20 @@ const HALF_LIFE_PERIOD = 5730;
  * dateSample('WOOT!') => false
  *
  */
-function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function dateSample(sampleActivity) {
+  let activity = parseFloat(sampleActivity);
+
+  if (!activity || typeof(sampleActivity) !== 'string' || activity > 15 || activity < 0) {
+    return false;
+  }
+  // 0.693 is an approximation of the natural logarithm of two
+  let k = 0.693 / HALF_LIFE_PERIOD;
+  // N0 / N
+  let x = MODERN_ACTIVITY / activity;
+
+  let age = Math.log(x) / k;
+
+  return Math.ceil(age);
 }
 
 module.exports = {
